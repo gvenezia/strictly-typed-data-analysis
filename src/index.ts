a70a7f412cc65db0583c8e2d08835443270920a4
Gaetano <g.venezia@outlook.com>
2019-10-05 16:58:53 -0700

    Add function to find # of wins

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,19 @@ const matchData = fs
   .split("\n")
   .map((line: string): string[] => line.split(","));
 
-console.log(matchData);
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D"
+}
+
+const findNumOfWins = (team: string): number => {
+  return matchData.filter(
+    line =>
+      (line[1] === team && line[5] === MatchResult.HomeWin) ||
+      (line[2] === team && line[5] === MatchResult.AwayWin)
+  ).length;
+};
+
+// console.log(matchData);
+console.log("Find", findNumOfWins("Chelsea"));
