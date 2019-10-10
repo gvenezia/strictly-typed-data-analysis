8ccf37fb5008f19ccdd60923d949037031f06f7b
Gaetano <g.venezia@outlook.com>
2019-10-09 21:08:02 -0700

    Add reportTarget + Configure ReportBuilder + Implement reportBuilder

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { parseCSV, formatData } from "./DataManipulations";
+import { ReportBuilder } from "./ReportBuilder";
 import { MatchResult } from "./enums";
 import { MatchData } from "./tuples";
+
+import { WinsByTeamAnalysis } from "./analyzers/WinsByTeamAnalysis";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
 
 const matchData = parseCSV("./football.csv");
 
 const formattedData = formatData(matchData);
 
-console.log(matchData);
-console.log("Find", findNumOfWins("Chelsea"));
+const reportBuilder = new ReportBuilder(
+  new WinsByTeamAnalysis("Man United"),
+  new ConsoleReport()
+);
+
+reportBuilder.buildAndPrintReport(formattedData);
